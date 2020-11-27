7817126b2766fd0435bd0eba43408b6943ac4ca8
Stefan <64359359+SteveMcT@users.noreply.github.com>
2020-11-27 19:14:30 +0100

    CHIF19Page
    Dear Community and friends
    
    This is the next bigger update for the CHIF19-Page
    It has some new featuers and especially a graphical overhaul...
    I hope you enjoy it and please write me if there is something to improve.

---
+++ chif19.at/CSS/main.js
@@ -0,0 +1,15 @@
+let height = $(window).scrollTop()
+let bodyHeight = $("body").height()
+
+$(document).scroll(function() {
+    height = $(window).scrollTop()
+    $("#chifNormal").css({
+        "opacity": 30 / height
+    })
+
+    $("#chifOrange").css({
+        "position": "relative",
+        "top": height - height / 5,
+        "opacity": 120 / height
+    })
+})
