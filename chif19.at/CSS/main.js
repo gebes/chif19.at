60db23296a97f7c5052090bd6eedcbc145a6e27d
Stefan <64359359+SteveMcT@users.noreply.github.com>
2020-12-03 10:02:42 +0100

    Achja
    der Chri hat gsagt, dass es sein kann, dass es bei mac über höhe null scrollen kann. Dyas is jez, denk ich draußen

--- chif19.at/CSS/main.js
+++ chif19.at/CSS/main.js
@@ -3,6 +3,7 @@ let bodyHeight = $("body").height()
 
 $(document).scroll(function() {
     height = $(window).scrollTop()
+
     $("#chifNormal").css({
         "opacity": 30 / height
     })
@@ -10,6 +11,12 @@ $(document).scroll(function() {
     $("#chifOrange").css({
         "position": "relative",
         "top": height - height / 5,
-        "opacity": 120 / height,
+        "opacity": 120 / height
     })
+
+    if(height<=0){
+        $("#chifOrange").css({
+            "opacity": 100
+        })
+    }
 })
